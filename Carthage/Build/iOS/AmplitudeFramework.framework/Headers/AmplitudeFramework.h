0ebc43562061c6ffed8e67e2cd12f5cdda0284c0
Colin Reisterer <creisterer@projectdecibel.com>
2016-02-22 21:05:49 -0500

    Add Carthage Support with Dynamic Framework and Shared Scheme

---
+++ Carthage/Build/iOS/AmplitudeFramework.framework/Headers/AmplitudeFramework.h
@@ -0,0 +1,19 @@
+//
+//  AmplitudeFramework.h
+//  AmplitudeFramework
+//
+//  Created by creisterer on 2/22/16.
+//  Copyright © 2016 Amplitude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AmplitudeFramework.
+FOUNDATION_EXPORT double AmplitudeFrameworkVersionNumber;
+
+//! Project version string for AmplitudeFramework.
+FOUNDATION_EXPORT const unsigned char AmplitudeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmplitudeFramework/PublicHeader.h>
+
+
